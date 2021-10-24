425fe14a4b084c85761d14ddecf15e6e2135d6e5
Ashish Kaushik <ashishkaushik53552@gmail.com>
2021-10-24 22:55:33 +0530

    "final"

--- client.c
+++ client.c
@@ -5,8 +5,10 @@ pthread_mutex_t mutex1=PTHREAD_MUTEX_INITIALIZER;
 float uti=0;
 pthread_t add_thread,checker,memory;
 struct Process{
+    int pid;
 	float duration;
 	float size;
+    time_t seconds;
 };
 struct Node{
 	struct Process * process;
@@ -24,8 +26,20 @@ struct Node* create_node(struct Process *process){
 }
 
 void take_input(){
-	printf("Enter physicalmem, reserved, rate,size,duration,totalTime, algo (1:first 2:best 3:next):\n");
-	scanf("%d%d%d%d%d%d%d",&p,&q,&n,&m,&t,&T,&a);
+	printf("Enter physical memory size:\n");
+	scanf("%d",&p);
+	printf("Enter reserved memory size:\n");
+	scanf("%d",&q);
+	printf("Enter process arrival rate:\n");
+	scanf("%d",&n);
+	printf("Size of the process in multiples of 10 MB:\n");
+	scanf("%d",&m);
+	printf("Enter duration of process in multiples of 5 seconds:\n");
+	scanf("%d",&t);
+	printf("Enter total time of execution in multiples of 100 seconds:\n");
+	scanf("%d",&T);
+	printf("Enter the algorithm to use: \n1 for first fit\n2 for best fit\n3 for next fit respectively\n");
+	scanf("%d",&a);
 	return;
 }
 void add_to_queue(struct Process *process){
@@ -46,161 +60,175 @@ struct Node* remove_from_queue(){
 	head=head->next;
 	return temp;
 }
-void *summon_process_and_add_in_queue_thread_function(void *arg){
-	while(1){
-		srand(time(0));
-		float r=(rand()%12) +1;
-		r/=10;
-		r*=n;
-		sleep(r);
-		float s=(rand()%26) +5;
-		s/=10;
-		s*=m;
-		float d=(rand()%56) +5;
-		d/=10;
-		d*=m;
-		struct Process *process=(struct Process *) malloc(sizeof (struct Process));
-		process->size=s;
-		process->duration=d;
-		pthread_mutex_lock(&mutex);
-		add_to_queue(process);
-		pthread_mutex_unlock(&mutex);
-
-	}
-}
-void *check_if_process_finished_function(void *arg){
-	while(1){
-		sleep(0.50);
-		pthread_mutex_lock(&mutex1);
-		for(int i=0;i<len;i++){
-			if(arr[i]>0) arr[i]-= (float)1/(float)2;
-			if(arr[i]>0) uti++;
-			else uti--; 
-		}
-		pthread_mutex_unlock(&mutex1);
-		printf("%f\n",uti*100/len);
-	}
-	return;
-}
-void *from_queue_add_to_memory_function(void *arg){
-	int nxt=0;
-	while(1){
-		if(head==NULL){	
-			continue;
-		}
-		pthread_mutex_lock(&mutex1);
-		struct Process * process=head->process;
-		float size=process->size;
-		float duration=process->duration;
-		if(a==1){// first-fit;
-			for(int i=0;i<len;i++){
-				if(arr[i]==0){
-					int t_size=0;
-					int j;
-					for(j=i;j<len;++j){
-						if(arr[j]==0){
-							t_size++;
-						}else{
-							break;
-						}
-					}
-					if(size<=t_size){
-						for(int k=i;k<j;k++) arr[k]=duration;
-						pthread_mutex_lock(&mutex);
-						remove_from_queue();
-						pthread_mutex_unlock(&mutex);
-					}
-					i=j;
-				}
-			}
-		}
-		if(a==2){
-			int s_l=0,s_r=0;
-			for(int i=0;i<len;i++){
-				if(arr[i]==0){
-					int t_size=0;
-					int j;
-					for(j=i;j<len;++j){
-						if(arr[j]==0){
-							t_size++;
-						}else{
-							break;
-						}
-					}
-					if(size<=t_size && (t_size<=(s_r-s_l))){
-						s_l=i;
-						s_r=j;
-					}	
-					i=j;
-				}
-			}
-			if(size<=(s_r-s_l) ){
-				for(int k=s_l;k<s_r;k++) arr[k]=duration;
-				pthread_mutex_lock(&mutex);
-				remove_from_queue();
-				pthread_mutex_unlock(&mutex);
-
-			}
-		}
-		if(a==3){
-			for(nxt;nxt<len;nxt++){
-				if(arr[nxt]==0){
-					int t_size=0;
-					int j;
-					for(j=nxt;j<len;++j){
-						if(arr[j]==0){
-							t_size++;
-						}else{
-							break;
-						}
-					}
-					if(size<=t_size){
-						for(int k=nxt;k<j;k++) arr[k]=duration;
-						nxt=j;
-						pthread_mutex_lock(&mutex);
-						remove_from_queue();
-						pthread_mutex_unlock(&mutex);
-						break;
-					}
-					
-				}
-			}
-			if(nxt==len) nxt=0;
-		}
-		pthread_mutex_unlock(&mutex1);
-	}
-}
-void summon_process_and_add_in_queue(){
-	int n=pthread_create(add_thread,NULL,summon_process_and_add_in_queue_thread_function,NULL);
-	if(n<0) printf("Error Creating thread\n");
-	return;
-};
-
-void check_if_process_finished(){
-	int n=pthread_create(checker,NULL,check_if_process_finished_function,NULL);
-	if(n<0) printf("Error Creating thread\n");
-	return;
-};
-void from_queue_add_to_memory(){
-	int n=pthread_create(memory,NULL,from_queue_add_to_memory_function,NULL);
-	if(n<0) printf("Error Creating thread\n");
-	return;
-};
 
 int main(){
-
+	time_t present,g_time;
+	time(&g_time);
 	take_input(p,q,m,n,t,T);
-	printf("%d%d%d%d%d%d",p,q,m,n,t,T);
 	len=p-q;
+	printf("Length of the available space %d MB\n",len);
 	arr=(float *)malloc(sizeof(float)*len);
 	for(int i=0;i<len;i++){
 		arr[i]=0;
 	}
-	summon_process_and_add_in_queue();
+	int pid=0;
+	int nxt=0;
+	while(1){
+		time(&present);
+		if(present-g_time> T*100){
+		printf("Time finished\n");
+		 exit(0);
+		}	
+		srand(time(0));
+		float r=(rand()%12) +1;
+		r/=10;
+		r*=n;
+		printf("Process rate %f\n",r);
+		sleep(r);
+		float s=(rand()%26) +5;
+		s/=10;
+		s*=m;
+		printf("Process size %f\n",s);
+		float d=(rand()%56) +5;
+		d/=10;
+		d*=t;
+		d*=5;
+		printf("Process duration %f\n",d);
+		struct Process *process=(struct Process *) malloc(sizeof (struct Process));
+		process->size=s;
+		pid++;
+		process->duration=d;
+		process->pid=pid;
+		time(&(process->seconds));
+		add_to_queue(process);
+		struct Node* temp=head;
+		
 
-	from_queue_add_to_memory();
+		if(temp==NULL){
+			continue;
+		}
+		struct Process *p_temp=temp->process;
+		float size=p_temp->size * 10;
+		float duration=p_temp->duration;
+		if(a==1){
+			for(int i=0;i<len;i++){
+					if(arr[i]==0){
+						int t_size=1;
+						int j=i;
+						while((j<len) && (arr[j]==0) && (t_size<=size)){
+							t_size++;
+							j++;
+						}
+						t_size--;
+						if(size<=t_size){
+							for(int k=i;k<j;k++) arr[k]=duration;
+							remove_from_queue();
+							time(&present);
+							printf("Average turnaround time for process %d is: %ld seconds\n",p_temp->pid,(present-p_temp->seconds));
+							break;
+						}
+						i=j;
+				}
+			}
+		}
+		else if(a==2){
+			float size=p_temp->size*10;
+			int s_l=0,s_r=INT_MAX;
+			for(int i=0;i<len;i++){
+					int t_size=1;
+					int j=i;
+					if(arr[i]==0){
+						while(arr[j]==0 && j<len){
+							t_size++;
+							j++;
+						}
+						t_size--;
+					
+						if(size<=t_size && t_size<(s_r-s_l)){
+							s_r=j;
+							s_l=i;
+						}
+						j=i-1;
+					}
+			}
+			if(s_r!=INT_MAX){
+				int t_size=0;
+				for(int i=s_l;i<s_r;i++){
+					t_size++;
+					arr[i]=p_temp->duration;
+					if(t_size==size) break;
+				}
+				remove_from_queue();
+				time(&present);
+				printf("Average turnaround time for process %d is: %ld seconds\n",p_temp->pid,(present-p_temp->seconds));
 
-	check_if_process_finished();
+			} 
+
+		}
+		if(nxt>=len) nxt=0;
+		else if(a==3){
+			int i=nxt;
+			int flag=0;
+			for(i;i<len;i++){
+				if(arr[i]==0){
+					int t_size=1;
+					int j=i;
+					while((j<len) && (arr[j]==0) && (t_size<=size)){
+						t_size++;
+						j++;
+					}
+					t_size--;
+					if(size<=t_size){
+						for(int k=i;k<j;k++) arr[k]=duration;
+						remove_from_queue();
+						time(&present);
+						flag=1;
+						printf("Average turnaround time for process %d is: %ld seconds\n",p_temp->pid,(present-p_temp->seconds));
+						nxt=i+1;
+						break;
+					}
+					i=j;
+				}
+			}
+			if(!flag){
+				for(int i=0;i<nxt;i++){
+					if(arr[i]==0){
+					int t_size=1;
+					int j=i;
+					while((j<len) && (arr[j]==0) && (t_size<=size)){
+						t_size++;
+						j++;
+					}
+					t_size--;
+					if(size<=t_size){
+						for(int k=i;k<j;k++) arr[k]=duration;
+						remove_from_queue();
+						time(&present);
+						flag=1;
+						printf("Average turnaround time for process %d is: %ld seconds\n",p_temp->pid,(present-p_temp->seconds));
+						nxt=i+1;
+						break;
+					}
+					i=j;
+				}
+				}
+
+			}
+		}
+		uti=0;
+		for(int i=0;i<len;i++){
+			if(arr[i]-r>0){
+				arr[i]-=r;
+				uti+=1;
+			}else{
+				arr[i]=0;
+			}
+		}
+		printf("Memory utilised is %f\n",(uti/len)*100);
+
+
+
+	}
 
     return 0;
 }
